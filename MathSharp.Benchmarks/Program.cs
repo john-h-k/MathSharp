dc0d34b46bcaad667c2cfdbf58d3b18a328619ac
Kevin Avignon <kevin.avignon@hypertherm.com>
2019-08-30 12:57:33 -0400

    Added benchmark project and BenchmarkDotNet dependency to solution.

---
+++ MathSharp.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace MathSharp.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
