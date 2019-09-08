b0a9a0e641d86786c4bc2c05b73105e9964a9a0a
Jacob <kozilord@gmail.com>
2019-09-08 13:43:32 +0200

    Add a test

---
+++ tests/MathSharp.UnitTests/VectorTests/VectorSingle/BasicMathsTests/Remainder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MathSharp.UnitTests.VectorTests.VectorSingle.BasicMathsTests
+{
+    public class Remainder
+    {
+        private readonly ITestOutputHelper output;
+
+        public Remainder(ITestOutputHelper helper)
+        {
+            output = helper;
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new[]
+            {
+                new object[] { 10.4f, 2f},
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Remainder_Theory(float left, float right)
+        {
+            var result = Vector.Remainder(Vector128.Create(left), right);
+            Vector.Store(result, out Vector4 l);
+            var r = new Vector4(left % right);
+
+            Assert.Equal(l, r);
+            output.WriteLine($"Expected: {r}");
+            output.WriteLine($"Found: {l}");
+        }
+    }
+}
