03231e1c5aa35498f885231ff3a0e1dc60a6f372
John <johnkellyoxford@gmail.com>
2019-09-17 22:22:06 +0100

    Fix inlining issue; Add intrinsic summary

--- MathSharp.Benchmarks/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace MathSharp.Benchmarks
-{
-    class Program
-    {
-        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-    }
-}
--- samples/MathSharp.Interactive/Program.cs
+++ samples/MathSharp.Interactive/Program.cs
@@ -16,7 +16,7 @@ namespace MathSharp.Interactive
     {
         private static void Main(string[] args)
         {
-            BenchmarkRunner.Run<RemainderBenchmark>();
+            Console.WriteLine(IntrinsicSupport.SupportSummary);
         }
 
         private static readonly Vector3 Direction = new Vector3(1, 2, 3);
