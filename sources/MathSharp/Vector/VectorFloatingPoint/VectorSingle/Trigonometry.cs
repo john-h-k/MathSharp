d1c90d1a5fffd131ac35ba405e45b9a80e610ea3
John <johnkellyoxford@gmail.com>
2019-10-06 17:17:33 +0100

    Fix TanApprox, cleanup trig

--- sources/MathSharp/Vector/VectorFloatingPoint/VectorSingle/Trigonometry.cs
+++ sources/MathSharp/Vector/VectorFloatingPoint/VectorSingle/Trigonometry.cs
@@ -32,11 +32,7 @@ namespace MathSharp
                 Vector128<float> neg = Subtract(tmp, vec);
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
-
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
-
-                vec = Or(select0, select1);
+                vec = Select(neg, vec, comp);
 
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
@@ -90,11 +86,7 @@ namespace MathSharp
                 Vector128<float> neg = Subtract(tmp, vec);
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
-
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
-
-                vec = Or(select0, select1);
+                vec = Select(neg, vec, comp);
 
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
@@ -137,15 +129,11 @@ namespace MathSharp
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
 
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
+                vec = Select(neg, vec, comp);
 
-                vec = Or(select0, select1);
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
-                select0 = And(comp, SingleConstants.One);
-                select1 = AndNot(comp, SingleConstants.NegativeOne);
-                vec = Or(select0, select1);
+                vec = Select(SingleConstants.NegativeOne, SingleConstants.One, comp);
 
                 // Polynomial approx
                 Vector128<float> cc0 = CosCoefficient0;
@@ -198,15 +186,11 @@ namespace MathSharp
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
 
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
-
-                vec = Or(select0, select1);
+                vec = Select(neg, vec, comp);
+                
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
-                select0 = And(comp, SingleConstants.One);
-                select1 = AndNot(comp, SingleConstants.NegativeOne);
-                vec = Or(select0, select1);
+                vec = Select(SingleConstants.NegativeOne, SingleConstants.One, comp);
 
                 // Fast polynomial approx
                 var cc1 = CosCoefficient1;
@@ -233,89 +217,109 @@ namespace MathSharp
         [MethodImpl(MaxOpt)]
         public static Vector128<float> Tan(Vector4FParam1_3 vector)
         {
-            var twoDivPi = PermuteWithW(TanConstants);
+            if (Sse.IsSupported)
+            {
+                var twoDivPi = PermuteWithW(TanConstants);
 
-            var tc0 = PermuteWithX(TanConstants);
-            var tc1 = PermuteWithY(TanConstants);
-            var epsilon = PermuteWithZ(TanConstants);
+                var tc0 = PermuteWithX(TanConstants);
+                var tc1 = PermuteWithY(TanConstants);
+                var epsilon = PermuteWithZ(TanConstants);
 
-            var va = Multiply(vector, twoDivPi);
-            va = Round(va);
+                var va = Multiply(vector, twoDivPi);
+                va = Round(va);
 
-            var vc = FastNegateMultiplyAdd(va, tc0, vector);
+                var vc = FastNegateMultiplyAdd(va, tc0, vector);
 
-            var vb = Abs(va);
+                var vb = Abs(va);
 
-            vc = FastNegateMultiplyAdd(va, tc1, vc);
+                vc = FastNegateMultiplyAdd(va, tc1, vc);
 
-            vb = ConvertToInt32(vb).AsSingle();
+                vb = ConvertToInt32(vb).AsSingle();
 
-            var vc2 = Multiply(vc, vc);
+                var vc2 = Multiply(vc, vc);
 
-            var t7 = PermuteWithW(TanCoefficients1);
-            var t6 = PermuteWithZ(TanCoefficients1);
-            var t4 = PermuteWithX(TanCoefficients1);
-            var t3 = PermuteWithW(TanCoefficients0);
-            var t5 = PermuteWithY(TanCoefficients1);
-            var t2 = PermuteWithZ(TanCoefficients0);
-            var t1 = PermuteWithY(TanCoefficients0);
-            var t0 = PermuteWithX(TanCoefficients0);
+                var t7 = PermuteWithW(TanCoefficients1);
+                var t6 = PermuteWithZ(TanCoefficients1);
+                var t4 = PermuteWithX(TanCoefficients1);
+                var t3 = PermuteWithW(TanCoefficients0);
+                var t5 = PermuteWithY(TanCoefficients1);
+                var t2 = PermuteWithZ(TanCoefficients0);
+                var t1 = PermuteWithY(TanCoefficients0);
+                var t0 = PermuteWithX(TanCoefficients0);
 
-            var vbIsEven = And(vb, SingleConstants.Epsilon).AsInt32();
-            vbIsEven = CompareEqual(vbIsEven, Vector128<int>.Zero);
+                var vbIsEven = And(vb, SingleConstants.Epsilon).AsInt32();
+                vbIsEven = CompareEqual(vbIsEven, Vector128<int>.Zero);
 
-            var n = FastMultiplyAdd(vc2, t7, t6);
-            var d = FastMultiplyAdd(vc2, t4, t3);
-            n = FastMultiplyAdd(vc2, n, t5);
-            d = FastMultiplyAdd(vc2, d, t2);
-            n = Multiply(vc2, n);
-            d = FastMultiplyAdd(vc2, d, t1);
-            n = FastMultiplyAdd(vc, n, vc);
+                var n = FastMultiplyAdd(vc2, t7, t6);
+                var d = FastMultiplyAdd(vc2, t4, t3);
+                n = FastMultiplyAdd(vc2, n, t5);
+                d = FastMultiplyAdd(vc2, d, t2);
+                n = Multiply(vc2, n);
+                d = FastMultiplyAdd(vc2, d, t1);
+                n = FastMultiplyAdd(vc, n, vc);
 
-            var nearZero = InBounds(vc, epsilon);
+                var nearZero = InBounds(vc, epsilon);
 
-            d = FastMultiplyAdd(vc2, d, t0);
+                d = FastMultiplyAdd(vc2, d, t0);
 
-            n = Select(n, vc, nearZero);
-            d = Select(d, SingleConstants.One, nearZero);
+                n = Select(n, vc, nearZero);
+                d = Select(d, SingleConstants.One, nearZero);
 
-            var r0 = Negate(n);
-            var r1 = Divide(n, d);
-            r0 = Divide(d, r0);
+                var r0 = Negate(n);
+                var r1 = Divide(n, d);
+                r0 = Divide(d, r0);
 
-            var isZero = CompareEqual(vector, Vector128<float>.Zero);
+                var isZero = CompareEqual(vector, Vector128<float>.Zero);
 
-            var result = Select(r0, r1, vbIsEven);
+                var result = Select(r0, r1, vbIsEven);
 
-            result = Select(result, Vector128<float>.Zero, isZero);
+                result = Select(result, Vector128<float>.Zero, isZero);
 
-            return result;
+                return result;
+            }
+
+            return SoftwareFallback(vector);
+
+            static Vector128<float> SoftwareFallback(Vector4FParam1_3 vector)
+            {
+                return Vector128.Create(
+                    MathF.Tan(X(vector)),
+                    MathF.Tan(Y(vector)),
+                    MathF.Tan(Z(vector)),
+                    MathF.Tan(W(vector))
+                );
+            }
         }
 
         private static readonly Vector128<float> TanEstCoefficients = Vector128.Create(2.484f, -1.954923183e-1f, 2.467401101f, ScalarSingleConstants.OneDivPi);
         [MethodImpl(MaxOpt)]
         public static Vector128<float> TanApprox(Vector4FParam1_3 vector)
         {
-            var oneDivPi = PermuteWithZ(TanEstCoefficients);
+            if (Sse.IsSupported)
+            {
+                var oneDivPi = PermuteWithW(TanEstCoefficients);
 
-            var v1 = Multiply(vector, oneDivPi);
-            v1 = Round(v1);
+                var v1 = Multiply(vector, oneDivPi);
+                v1 = Round(v1);
 
-            v1 = FastNegateMultiplyAdd(SingleConstants.Pi, v1, vector);
+                v1 = FastNegateMultiplyAdd(SingleConstants.Pi, v1, vector);
 
-            var t0 = PermuteWithX(TanEstCoefficients);
-            var t1 = PermuteWithY(TanEstCoefficients);
-            var t2 = PermuteWithZ(TanEstCoefficients);
+                var t0 = PermuteWithX(TanEstCoefficients);
+                var t1 = PermuteWithY(TanEstCoefficients);
+                var t2 = PermuteWithZ(TanEstCoefficients);
 
-            var v2T2 = FastNegateMultiplyAdd(v1, v1, t2);
-            var v2 = Multiply(v1, v1);
-            var v1T0 = Multiply(v1, t0);
-            var v1T1 = Multiply(v1, t1);
+                var v2T2 = FastNegateMultiplyAdd(v1, v1, t2);
+                var v2 = Multiply(v1, v1);
+                var v1T0 = Multiply(v1, t0);
+                var v1T1 = Multiply(v1, t1);
 
-            var d = ReciprocalApprox(v2T2);
-            var n = FastMultiplyAdd(v2, v1T1, v1T0);
+                var d = ReciprocalApprox(v2T2);
+                var n = FastMultiplyAdd(v2, v1T1, v1T0);
 
-            return Multiply(n, d);
+                return Multiply(n, d);
+            }
+
+            return Tan(vector);
         }
 
         [MethodImpl(MaxOpt)]
@@ -334,15 +338,12 @@ namespace MathSharp
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
 
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
+                vec = Select(neg, vec, comp);
 
-                vec = Or(select0, select1);
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
-                select0 = And(comp, SingleConstants.One);
-                select1 = AndNot(comp, SingleConstants.NegativeOne);
-                var cosVec = Or(select0, select1);
+                var cosVec = Select(SingleConstants.NegativeOne, SingleConstants.One, comp);
+
 
                 // Polynomial approx
                 Vector128<float> sc0 = SinCoefficient0;
@@ -414,15 +415,11 @@ namespace MathSharp
 
                 Vector128<float> comp = CompareLessThanOrEqual(abs, SingleConstants.PiDiv2);
 
-                Vector128<float> select0 = SelectWhereTrue(vec, comp);
-                Vector128<float> select1 = SelectWhereFalse(neg, comp);
-
-                vec = Or(select0, select1);
+                vec = Select(neg, vec, comp);
                 Vector128<float> vectorSquared = Multiply(vec, vec);
 
-                select0 = And(comp, SingleConstants.One);
-                select1 = AndNot(comp, SingleConstants.NegativeOne);
-                var cosVec = Or(select0, select1);
+                var cosVec = Select(SingleConstants.NegativeOne, SingleConstants.One, comp);
+
 
                 // Fast polynomial approx
                 var sc1 = SinCoefficient1;
